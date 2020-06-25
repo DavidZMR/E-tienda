c72463e1328f7470c641d73fd26fe016ad9f7903
blank-mpr <jor_manuel186@hotmail.com>
2020-06-25 15:21:49 -0500

    Reduje tamaño del footer, ajuste el login y el register en el nav con Routing ;)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { TiendaComponent } from './tienda/tienda/tienda.component';
-import {ContactoComponent} from './contacto/contacto.component';
+import { ContactoComponent } from './contacto/contacto.component';
 import { AcercadeComponent } from './acercade/acercade.component';
 import { PreguntasComponent } from './preguntas/preguntas.component';
 import { RegistroComponent } from './registro/registro.component';
@@ -14,19 +14,20 @@ import { LoginComponent } from './componentes/login/login/login.component';
 
 const routes: Routes = [
 
-   {path: 'home', component:HomeComponent},
-   {path: 'tienda', component:TiendaComponent},
-   {path: 'contacto', component:ContactoComponent},
-   {path: 'acercade', component:AcercadeComponent},
-   {path: 'preguntas', component:PreguntasComponent},
-   {path: 'registro', component:RegistroComponent},
-   {path: 'ingreso', component:IngresoComponent},
-   {path: 'articulo/:id', component:ArticuloComponent},
-  {path: 'buscador/:nombre', component:BuscadorComponent},
-   {path: '**', pathMatch:'full', redirectTo: 'home'}
-    //{path: 'login', component: LoginComponent},
-  // {path: 'register', component: RegisterComponent}
-  
+  { path: 'home', component: HomeComponent },
+  { path: 'tienda', component: TiendaComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'acercade', component: AcercadeComponent },
+  { path: 'preguntas', component: PreguntasComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'ingreso', component: IngresoComponent },
+  { path: 'articulo/:id', component: ArticuloComponent },
+  { path: 'buscador/:nombre', component: BuscadorComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+
 ];
 
 @NgModule({
