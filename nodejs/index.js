19a5b6514e314d53b490d2440dd6c7048dc516f2
blank-mpr <jor_manuel186@hotmail.com>
2020-06-26 11:52:06 -0500

    Agregando cosas del email (contacto)

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); 
 const misRouters = require('./rutas/ruta');
-
+// const nodemail= require('nodemailer');
 const app = express();
 app.use(cors());
+
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
@@ -13,6 +15,11 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 app.use('/', misRouters);
+
+//MAIL
+app.post('/enviar', function(req, res){
+    misRouters.enviar
+  });
 
 app.get('/', function (req, res) {
     res.send('Hello World');
--- nodejs/rutas/ruta.js
+++ nodejs/rutas/ruta.js
@@ -1,8 +1,46 @@
 const express = require('express');
+
 const router = express.Router();
 
-router.get('/check', (req,res) =>{
+router.get('/check', (req, res) => {
     res.send({ message: 'Hola Mundo, en que puedo ayudarte??' })
 });
 
-module.exports = router; 
+
+//MAIL 
+router.enviar = function (req, res) {
+    var nodemailer = require('nodemailer');
+    // email sender function
+    exports.enviar = function (req, res) {
+        // Definimos el transporter
+        var transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            auth: {
+                user: 'jormanuel202@gmail.com',
+                pass: '9942566jorge'
+            }
+        });
+        // Definimos el email
+        var mailOptions = {
+            from: 'jormanuel202 <jormanuel202@gmail.com>',
+            to: req.body.email,
+            subject: req.body.asunto,
+            text: req.body.mensaje
+        };
+        // Enviamos el email
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log("Correo no enviado");
+                // res.render('error',{titulo: "Error al enviar el Correo"});
+                res.send(500, err.message);
+            } else {
+                console.log("Correo enviado");
+                // res.render('enviado',{titulo: "Correo Enviado"});
+                res.status(200).jsonp(req.body);
+            }
+        });
+    };
+}
+
+module.exports = router;
+
